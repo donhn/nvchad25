3f9cd5469f17164642dabe897a89cb9f3dc8f70d
don.nguyen <don.nguyen@astrodigital.com>
2024-03-27 11:47:26 -0700

    formatted

--- chadrc.lua
+++ chadrc.lua
@@ -1,32 +1,32 @@
-require("autocmds")
+require "autocmds"
 
 ---@type ChadrcConfig
 local M = {}
 
 M.ui = {
-	statusline = {
-		theme = "vscode_colored",
-		separator_style = "arrow",
-	},
+  statusline = {
+    theme = "vscode_colored",
+    separator_style = "arrow",
+  },
 
-	theme = "gruvbox",
-	hl_override = {
-		IndentBlanklineContextChar = {
-			fg = "#d5c4a1",
-		},
-		IndentBlanklineContextStart = {
-			bg = "#282828",
-		},
-		DiffChange = {
-			fg = "#fabd2f",
-		},
-		Comment = {
-			fg = "#bdae93",
-			italic = true,
-		},
-	},
+  theme = "gruvbox",
+  hl_override = {
+    IndentBlanklineContextChar = {
+      fg = "#d5c4a1",
+    },
+    IndentBlanklineContextStart = {
+      bg = "#282828",
+    },
+    DiffChange = {
+      fg = "#fabd2f",
+    },
+    Comment = {
+      fg = "#bdae93",
+      italic = true,
+    },
+  },
 
-	transparency = true,
+  transparency = true,
 }
 
 return M
